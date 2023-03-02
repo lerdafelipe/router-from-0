ea614b7d7b5c8a4d7e5780cd87a051b3d23c85ed
Felipe Lerda <lerdafelipe@gmail.com>
2023-03-02 15:49:28 +0100

    SPAs TASK  DONE

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - ✅ Instalar el linter
 - ✅ Crear una forma de hacer MPAs (Multiple Page Application)
-- [] Crea una forma de hacer SPAs (Single Page Applications)
+- ✅ Crea una forma de hacer SPAs (Single Page Applications)
 - [] Poder navegar entre páginas con el botón de atrás
 - [] Crear componente Link para hacerlo declarativo
 - [] Crear componente Router para hacerlo más declarativo
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+const navigate = (href) => {
+  window.history.pushState({}, '', href)
+  // create a custom event
+  const navigateEvent = new Event('pushState')
+  window.dispatchEvent(navigateEvent)
+}
 
 const HomePage = () => {
   return (
-    <h1>This is our home page in a react router project</h1>
+    <main>
+      <h1>This is our home page in a react router project</h1>
+      <button onClick={() => navigate('/about')}>Go to About me</button>
+    </main>
   )
 }
 
 const AboutPage = () => {
   return (
-    <h1>This is our About page, I am the better developer for your bussines</h1>
+    <main>
+      <h1>This is our About page, I am the better developer for your bussines</h1>
+      <button onClick={() => navigate('/')}>Go to Home page</button>
+    </main>
   )
 }
 
 const App = () => {
-  const [currentPath] = useState(window.location.pathname)
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener('pushState', onLocationChange)
+
+    return () => window.removeEventListener('pushState', onLocationChange)
+  }, [])
 
   return (
     <div>
